24eac64a4927e0c92ac6825625cf9ee6c273dff7
prajanlal <prajanswags@gmail.com>
2025-08-29 22:59:48 +0530

    ----

--- programs/program/Gui.java
+++ programs/program/Gui.java
@@ -1,23 +1,22 @@
 package programs.program;
 
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Gui {
     public static void main(String[] args) {
-      JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,550);
-        frame.setVisible(true);
-        
-        JPanel panel = new JPanel();
-        panel.setBounds(20, 020, 040, 0150);
-        
 
-        JTextField textField = new JTextField();
-        textField.setBounds(20, 40, 50, 50);
-        panel.add(textfield);
+       JFrame frame = new JFrame();
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setTitle("Our GUI");
+       frame.setSize(2000, 3000);
+       frame.setVisible(true);
+       
     }
 }
-
