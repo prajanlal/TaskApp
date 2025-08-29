b645a8a40a83e6062b7c045205e4046f9686a7b2
prajanlal <prajanswags@gmail.com>
2025-08-29 22:16:12 +0530

    Added gui components

---
+++ programs/program/Gui.java
@@ -0,0 +1,14 @@
+package programs.program;
+
+import javax.swing.JFrame;
+
+public class Gui {
+    public static void main(String[] args) {
+      JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,550);
+        frame.setVisible(true);
+
+    }
+}
+
